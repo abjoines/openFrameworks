0a1541bc4d58796e27e0a263104d930c9d695f79
abjoines <joina778@newschool.edu>
2019-01-29 18:17:50 -0500

    first commit

---
+++ Project-1/Project-1/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Project-1
+//
+//  Created by Alyssa Joines on 1/29/19.
+//  Copyright © 2019 Alyssa Joines. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+    for(int x=0; x<100; x++){
+        if(x%2 == 1 && x!= 63){
+            
+            cout << x << endl;
+            
+        }
+    }
+}
